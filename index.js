2137f34426642d8445f1eb3df315f8a0b59af796
Julius Brian Haldar <jbhaldar1234@gmail.com>
2022-06-02 15:20:24 +0600

    On Hover Effects

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ var mybutton = document.getElementById("myBtn");
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+  if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
